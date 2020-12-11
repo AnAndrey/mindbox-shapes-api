9a53f8a7c3cbe3aec5d1485b9619a6d06e94ff9b
Boris Pantiukhin <pantboris@gmail.com>
2020-12-11 19:05:33 +0300

    feat(api): implement controller

---
+++ Shapes/Controllers/FigureController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Shapes.Models;
+using Shapes.Shapes;
+
+namespace Shapes.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FigureController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var shapes = new List<Shape> {new Circle {Id = id, Radius = 3.33M}};
+            var r = shapes.Select(s => new ShapeModel
+                {Description = s.GetType().Name, Square = s.Square.ToString("F")});
+            return Ok(r);
+        }
+    }
+}
---
+++ Shapes/Models/ShapeModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Shapes.Models
+{
+    public class ShapeModel
+    {
+        public string Description { get; set; }
+
+        public string Square { get; set; }
+    }
+}

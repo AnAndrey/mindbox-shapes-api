c4a52d4bc246559013dd54462a5a6dbb678207ea
Boris Pantiukhin <pantboris@gmail.com>
2020-12-18 18:37:22 +0300

    feat: implement rectangle

---
+++ Shapes/Shapes/Rectangle.cs
@@ -0,0 +1,17 @@
+﻿namespace Shapes.Shapes
+{
+    public class Rectangle : Shape
+    {
+        public double Side1 { get; set; }
+
+        public double Side2 { get; set; }
+
+        public Rectangle(int id, double side1, double side2) : base(id)
+        {
+            Side1 = side1;
+            Side2 = side2;
+        }
+
+        public override double Square => SquareService.CalculateSquare(this);
+    }
+}

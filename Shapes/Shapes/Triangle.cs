9110598f1a5efaa28af574989f0992e821f22b3c
Boris Pantiukhin <pantboris@gmail.com>
2020-12-18 18:30:06 +0300

    feat: implement triangle

---
+++ Shapes/Shapes/Triangle.cs
@@ -0,0 +1,20 @@
+﻿namespace Shapes.Shapes
+{
+    public class Triangle : Shape
+    {
+        public double Side1 { get; set; }
+
+        public double Side2 { get; set; }
+
+        public double Side3 { get; set; }
+
+        public Triangle(int id, double side1, double side2, double side3) : base(id)
+        {
+            Side1 = side1;
+            Side2 = side2;
+            Side3 = side3;
+        }
+
+        public override double Square => SquareService.CalculateSquare(this);
+    }
+}

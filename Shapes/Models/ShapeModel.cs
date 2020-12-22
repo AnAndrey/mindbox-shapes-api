92a852c55b1a94558a5af63c34eb211974508217
Boris Pantiukhin <pantboris@gmail.com>
2020-12-22 16:36:49 +0300

    feat(api): implement validation

--- Shapes/Models/ShapeModel.cs
+++ Shapes/Models/ShapeModel.cs
@@ -1,11 +1,31 @@
-﻿namespace Shapes.Models
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Dynamic;
+using System.Linq;
+using Shapes.Common;
+
+namespace Shapes.Models
 {
-    public class ShapeModel
+    public class ShapeModel : IValidatableObject
     {
         public int? Id { get; set; }
 
+        [Required]
         public string ShapeType { get; set; }
 
-        public object Metadata { get; set; }
+        [Required]
+        public ExpandoObject Metadata { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            var types = new[] { ShapesTypes.Circle, ShapesTypes.Triangle, ShapesTypes.Rectangle };
+            if (!types.Contains(ShapeType, StringComparer.InvariantCultureIgnoreCase))
+            {
+                yield return new ValidationResult(
+                    $"Invalid shape type: {ShapeType}",
+                    new[] {nameof(ShapeType)});
+            }
+        }
     }
 }
